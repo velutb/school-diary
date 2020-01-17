bc3e087c21ce394e722a498a7d931d7c4e9e0fce
velutb <59138071+velutb@users.noreply.github.com>
2020-01-17 19:04:45 +0300

    Update urls.py
    Added getting a timetable via a get-request

--- school_diary/timetable/urls.py
+++ school_diary/timetable/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.timetable, name='timetable'),
+    path('get', views.get, name='timetable_get'),
     path('download/', views.download, name='timetable_download'),
 ]
