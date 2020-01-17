e6b13d629241301389e8fd24e8dbb29583da7206
velutb <59138071+velutb@users.noreply.github.com>
2020-01-17 19:02:23 +0300

    Update views.py
    Added getting a timetable via a get-request

--- school_diary/timetable/views.py
+++ school_diary/timetable/views.py
@@ -41,3 +41,25 @@ def download(request):
     return render(request, 'timetable_download.html')
 
 
+def get(request):
+    chosen_litera = request.GET.get("l",'')
+    chosen_grade = request.GET.get("g",0)
+    chosen_class = str(chosen_grade) + chosen_litera
+    try:
+        my_grade = Grades.objects.get(number=chosen_grade, letter=chosen_litera)
+        lessons_list_monday = Lessons.objects.filter(connection=my_grade.id, day="Понедельник")
+        lessons_list_tuesday = Lessons.objects.filter(connection=my_grade.id, day="Вторник")
+        lessons_list_wednesday = Lessons.objects.filter(connection=my_grade.id, day="Среда")
+        lessons_list_thursday = Lessons.objects.filter(connection=my_grade.id, day="Четверг")
+        lessons_list_friday = Lessons.objects.filter(connection=my_grade.id, day="Пятница")
+        lessons_list_saturday = Lessons.objects.filter(connection=my_grade.id, day="Суббота")
+        return render(request, 'timetable_list.html', {
+            'my_grade': chosen_class,
+            'monday': lessons_list_monday,
+            'tuesday': lessons_list_tuesday,
+            'wednesday': lessons_list_wednesday,
+            'thursday': lessons_list_thursday,
+            'friday': lessons_list_friday,
+            'saturday': lessons_list_saturday})
+    except Exception as message:
+        return HttpResponse("Ваш класс не найден в базе данных. Ошибка: %s" % message)
