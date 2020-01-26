1ddce64524b0fc84df158f9c64d204dea1c0439a
Pashs-ba <pashs.ba@gmail.com>
2020-01-26 15:19:03 +0300

    return users to dairy, fixed legacy

--- school_diary/diary/models.py
+++ school_diary/diary/models.py
@@ -1,7 +1,6 @@
+from django.contrib.auth.base_user import AbstractBaseUser
 from django.db import models
 from datetime import date
-
-from ...school_diary.main.models import Subjects, Students, Grades
 
 
 GRADES = [
@@ -31,7 +30,7 @@ LITERAS = [
 ]
 
 
-WEIGHTS = [
+MARKS = [
     (5, "5 - отлично"),
     (4, "4 - хорошо"),
     (3, "3 - удовлетворительно"),
@@ -40,64 +39,64 @@ WEIGHTS = [
 ]
 
 
-# class Subjects(models.Model):
-#     name = models.CharField(max_length=50, verbose_name="Название")
-#
-#     class Meta:
-#         ordering = ['name']
-#         verbose_name = "Предмет"
-#         verbose_name_plural = "Предметы"
-#
-#     def __str__(self):
-#         return self.name
+class Subjects(models.Model):
+    name = models.CharField(max_length=50, verbose_name="Название")
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = "Предмет"
+        verbose_name_plural = "Предметы"
+
+    def __str__(self):
+        return self.name
 
 
-# class Teachers(models.Model):
-#     first_name = models.CharField(max_length=50, verbose_name="Имя")
-#     surname = models.CharField(max_length=50, verbose_name="Фамилия")
-#     second_name = models.CharField(max_length=50, verbose_name="Отчество", blank=True)
-#     subjects = models.ManyToManyField(Subjects, verbose_name="Предметы")
-#     login = models.CharField(max_length=50, verbose_name="Логин")
-#     password = models.CharField(max_length=50, verbose_name="Пароль")
-#
-#     class Meta:
-#         ordering = ['surname', 'first_name', 'second_name']
-#         verbose_name = "Учитель"
-#         verbose_name_plural = "Учителя"
-#
-#     def __str__(self):
-#         return self.surname + " " + self.first_name + " " + self.second_name
-#
-#
-# class Grades(models.Model):
-#     number = models.IntegerField(choices=GRADES, verbose_name="Класс")
-#     letter = models.CharField(max_length=2, verbose_name="Буква")
-#     main_teacher = models.ForeignKey(Teachers, null=True, on_delete=models.SET_NULL, verbose_name="Классный руководитель")
-#
-#     class Meta:
-#         ordering = ['number', 'letter']
-#         verbose_name = "Класс"
-#         verbose_name_plural = "Классы"
-#
-#     def __str__(self):
-#         return str(self.number) + self.letter
+class Teachers(AbstractBaseUser):
+    first_name = models.CharField(max_length=50, verbose_name="Имя")
+    surname = models.CharField(max_length=50, verbose_name="Фамилия")
+    second_name = models.CharField(max_length=50, verbose_name="Отчество", blank=True)
+    subjects = models.ManyToManyField(Subjects, verbose_name="Предметы")
+    password = models.CharField(max_length=50, verbose_name="Пароль")
+    email = models.CharField(max_length=50, verbose_name="Почта")
+
+    class Meta:
+        ordering = ['surname', 'first_name', 'second_name']
+        verbose_name = "Учитель"
+        verbose_name_plural = "Учителя"
+
+    def __str__(self):
+        return '{} {} {}'.format(self.surname, self.first_name, self.second_name)
 
 
-# class Students(models.Model):
-#     first_name = models.CharField(max_length=50, verbose_name="Имя")
-#     surname = models.CharField(max_length=50, verbose_name="Фамилия")
-#     second_name = models.CharField(max_length=50, verbose_name="Отчество", blank=True)
-#     login = models.CharField(max_length=50, verbose_name="Логин")
-#     password = models.CharField(max_length=50, verbose_name="Пароль")
-#     grade = models.ForeignKey(Grades, on_delete=models.CASCADE, verbose_name="Класс")
-#
-#     class Meta:
-#         ordering = ['surname', 'first_name', 'second_name']
-#         verbose_name = "Учитель"
-#         verbose_name_plural = "Учителя"
-#
-#     def __str__(self):
-#         return self.surname + " " + self.first_name + " " + self.second_name
+class Grades(models.Model):
+    number = models.IntegerField(choices=GRADES, verbose_name="Класс")
+    letter = models.CharField(max_length=2, verbose_name="Буква")
+    main_teacher = models.ForeignKey(Teachers, null=True, on_delete=models.SET_NULL, verbose_name="Классный руководитель")
+
+    class Meta:
+        ordering = ['number', 'letter']
+        verbose_name = "Класс"
+        verbose_name_plural = "Классы"
+
+    def __str__(self):
+        return '{}{}'.format(self.number, self.letter)
+
+
+class Students(AbstractBaseUser):
+    first_name = models.CharField(max_length=50, verbose_name="Имя")
+    surname = models.CharField(max_length=50, verbose_name="Фамилия")
+    second_name = models.CharField(max_length=50, verbose_name="Отчество", blank=True)
+    password = models.CharField(max_length=50, verbose_name="Пароль")
+    grade = models.ForeignKey(Grades, on_delete=models.CASCADE, verbose_name="Класс")
+    email = models.CharField(max_length=50, verbose_name="Почта")
+
+    class Meta:
+        ordering = ['surname', 'first_name', 'second_name']
+        verbose_name = "Учитель"
+        verbose_name_plural = "Учителя"
+
+    def __str__(self):
+        return '{} {} {}'.format(self.surname, self.first_name, self.second_name)
 
 
 class HomeTasks(models.Model):
@@ -113,13 +112,13 @@ class HomeTasks(models.Model):
         verbose_name_plural = "Домашние задания"
 
     def __str__(self):
-        return "Д/з на " + str(self.date_to_make)
+        return "Д/з на {}".format(self.day_to_make)
 
 
 class Marks(models.Model):
     subject = models.ForeignKey(Subjects, on_delete=models.CASCADE, verbose_name="Предмет")
     creation_date = models.DateField(default=date.today, verbose_name="Дата")
-    weight = models.IntegerField(choices=WEIGHTS, verbose_name="Вес оценки")
+    mark = models.IntegerField(choices=MARKS, verbose_name="оценка")
     student = models.ForeignKey(Students, on_delete=models.CASCADE, verbose_name="Ученик")
     grade = models.ForeignKey(Grades, on_delete=models.CASCADE, verbose_name="Класс")
     comment = models.CharField(max_length=150, verbose_name="Комментарий", blank=True)
