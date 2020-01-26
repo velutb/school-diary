f02c29935d787289dbdb8773bdfab259d28bdd32
Pashs-ba <pashs.ba@gmail.com>
2020-01-26 22:41:42 +0300

    fix some bug

--- school_diary/diary/views.py
+++ school_diary/diary/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.shortcuts import render, HttpResponse
 from urllib.parse import unquote
 from .forms import StudentRegistration
+from .models import Students
 from django.contrib.auth import authenticate
 
 
@@ -9,8 +10,11 @@ def index(request):
     if request.method == 'POST':
         form = StudentRegistration(request.POST)
         if form.is_valid():
-            user = authenticate()
-            return HttpResponse('True')
+            # if user is None:
+            #     return HttpResponse('True')
+            # else:
+            #     return HttpResponse('False')
+            return HttpResponse(form.cleaned_data['email'])
     else:
         form = StudentRegistration()
         return render(request, 'registration.html', {'form':form})
